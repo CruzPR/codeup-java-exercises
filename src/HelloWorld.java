e133c58132609841989cbbbb3a7175c81cc023fc
Carlos Cruz <cruzprww2@gmail.com>
2022-06-06 12:13:09 -0500

    Continued working on the curriculum exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,11 +2,25 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        // 1:
         int myFavoriteNumber = 7;
         System.out.println(myFavoriteNumber);
-
+        // 2:
         String myString = "Carlos Cruz";
         System.out.println(myString);
+        // 3:
+        // Can't mutate myString, strings in Java are immutable.
+        // 4:
+        // Cannot add a number data type to a String data type.
+        // 5:
+        // 6:
+        // 7:
+        // 8:
+        // 9:
+        float myNumber = 3.14F; // Added F at the end.
+        System.out.println(myNumber);
+
+
 
 
 
