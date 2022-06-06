1f3869cb0dd35d2932c3fb9a8274540ecbc4696b
Carlos Cruz <cruzprww2@gmail.com>
2022-06-06 11:59:07 -0500

    Started working on the curriculum exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Carlos Cruz";
+        System.out.println(myString);
+
+
+
     }
 
 }
+
