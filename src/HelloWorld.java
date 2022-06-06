14de5eb831d3841b3c37c2747843f58791b63f25
Carlos Cruz <cruzprww2@gmail.com>
2022-06-06 12:22:29 -0500

    Continued working on the curriculum exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -19,7 +19,20 @@ public class HelloWorld {
         // 9:
         float myNumber = 3.14F; // Added F at the end.
         System.out.println(myNumber);
-
+        // 10:
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        // 11:
+        // String class = "hello";
+        // I can't create variable using a reserve Java keyword.
+        // 12:
+        //
+        // 13:
+        // 14:
 
 
 
