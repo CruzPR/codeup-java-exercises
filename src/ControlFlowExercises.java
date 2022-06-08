372f9c90258262d8e88a2572bf8a849b4a0b4167
Carlos Cruz <cruzprww2@gmail.com>
2022-06-08 14:26:57 -0500

    working on ControlFlowExercises.java

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,32 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
+//        int num = 5;
+//        while(num <= 15) {
+//            System.out.println(num + " ");
+//            num += 1;
+//        }
+//        int num = 0;
+//        do {
+//            System.out.println(num);
+//            num += 2;
+//        } while( num < 100);
+        int num = 100;
+        do {
+            System.out.println(num);
+            num -= 5;
+        } while( num >= -10);
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
