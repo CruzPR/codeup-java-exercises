f978ca7b00fc55780a4509c383be18f5173db8db
Carlos Cruz <cruzprww2@gmail.com>
2022-06-08 15:31:01 -0500

    working on ControlFlowExercises.java

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
@@ -11,19 +13,63 @@ public class ControlFlowExercises {
 //            System.out.println(num);
 //            num += 2;
 //        } while( num < 100);
-        int num = 100;
-        do {
-            System.out.println(num);
-            num -= 5;
-        } while( num >= -10);
+//        int num = 100;
+//        do {
+//            System.out.println(num);
+//            num -= 5;
+//        } while( num >= -10);
 
+//    int num = 2;
+//    do {
+//        System.out.println(num);
+//        num *= num;
+//    } while (num < 1_000_000);
 
+//        int num = 100;
+//        do {
+//            System.out.println(num);
+//            num -= 5;
 
+//        } while(num >= -10);
+//         for (int i = 100; i >= -10; i -=5) {
+//             System.out.println(i);
+//         }
 
+//        for(int i = 1; i <= 100; i += 1) {
+//            if(i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//                continue;
+//            }
+//            if(i % 3 == 0) {
+//                System.out.println("Fizz");
+//                continue; // Jumps to the next iteration instead
+//                // of printing out 3 and so on
+//            }
+//            if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+//        boolean willContinue = false;
+//    do {
+//        System.out.println("Enter an integer"); // ask for input
+//        Scanner sc = new Scanner(System.in); //import scanner, get user input
+//        int userInput = sc.nextInt(); // grab the integer
+////        System.out.println(userInput + " | " + userInput * userInput + " | " + Math.pow(userInput, 3)); // print the input
+//        for (int i = 0; i <= userInput; i++) {
+//            System.out.println(userInput + " | " + i * i + " | " + Math.pow(i, 3));
+//        }
+//        System.out.println("Do you want to enter another number? (y/n)");
+//        String userResponse = sc.next().toLowerCase();
+//        if(userResponse.equals("y")) {
+//            willContinue = true;
+//        } else {
+//            willContinue = false;
+//        }
+//    } while (willContinue);
 
-
-
-
+        Scanner sc = new Scanner(System.in)
 
 
 
